eeb95cb2681a97478e14f6a3208508d1931e1d22
mergegi <128318777+mergegi@users.noreply.github.com>
2024-11-10 14:14:47 -0500

    complete candy cane problem

---
+++ contest/candyCane.py
@@ -0,0 +1,36 @@
+# import sys
+# sys.stdin = open("candyCane.in")
+
+# read input
+N, M = [int(w) for w in input().strip().split()]
+cows = [int(w) for w in input().strip().split()]
+candies = [int(w) for w in input().strip().split()]
+
+# return how much the cow eat
+def eat(cowHeight, candyHeight, candyBase):
+    if cowHeight > candyBase:
+        if cowHeight < candyHeight:
+            return cowHeight - candyBase
+        else: 
+            return candyHeight - candyBase
+    else: 
+        return 0
+    
+for i in range(M): # loop for candies
+    candyBase = 0
+    candyHeight = candies[i]
+    for j in range(N): # loop for cows
+        cowHeight = cows[j]
+        if cowHeight <= candyBase:
+            continue
+        
+        amountEaten = eat(cowHeight, candyHeight, candyBase) 
+
+        cows[j] += amountEaten
+        candyBase += amountEaten
+        if candyBase >= candyHeight:
+            break
+        
+
+for cow in cows:
+    print(cow)
