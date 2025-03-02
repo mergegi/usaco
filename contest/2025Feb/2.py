c3d3311e9c6af3562a32a89e6a1b39beaad554fa
mergegi <128318777+mergegi@users.noreply.github.com>
2025-03-02 15:16:14 -0500

    Improve Feb20252 and Dec2024

---
+++ contest/2025Feb/2.py
@@ -0,0 +1,25 @@
+#import sys 
+#sys.stdin = open('2.in')
+
+N = int(input().strip())
+a = [int(e) for e in input().strip().split()]
+
+times = [0] * (N + 1)
+for n in a:
+    times[n] += 1
+
+
+op1 = 0
+for i in range(N + 1):
+    ans = 0
+    mex = i
+    
+    if times[mex] > op1:
+        ans = times[mex]
+    else:
+        ans = op1
+    
+    if times[mex] == 0:
+        op1 += 1
+    
+    print(ans)
